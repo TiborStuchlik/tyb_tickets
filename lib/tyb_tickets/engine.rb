210adc44e2dbeff4ec29265e4498db2a54b1c122
Tibor stuchlík <tibor@stuchlik.info>
2021-07-20 09:54:45 +0200

    first

--- lib/tyb_tickets/engine.rb
+++ lib/tyb_tickets/engine.rb
@@ -1,5 +1,9 @@
 module TybTickets
   class Engine < ::Rails::Engine
     isolate_namespace TybTickets
+
+    initializer "static assets" do |app|
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/html")
+    end
   end
 end
